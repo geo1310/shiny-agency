bd4100143df382ca6e718856eef2e628aa3e483a
Georges Briche <gbriche59@yahoo.fr>
2023-05-05 17:29:21 +0200

    Mise a jour code

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/'
 import Survey from './pages/Survey'
+import Header from './components/Header'
+import Error from './components/Error'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/survey" element={<Survey />} />
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   </React.StrictMode>,
