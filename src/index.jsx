2e824d0b210c89153a682b08fae27ff501b5133a
Georges Briche <gbriche59@yahoo.fr>
2023-05-05 19:19:36 +0200

    Mise a jour code

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/'
 import Survey from './pages/Survey'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
 import Header from './components/Header'
 import Error from './components/Error'
 
