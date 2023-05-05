91b86c46f8ce7bdd5cba5541a8b0bc6c3e030aa3
Georges Briche <gbriche59@yahoo.fr>
2023-05-05 18:23:29 +0200

    Mise a jour code

--- src/index.jsx
+++ src/index.jsx
@@ -13,6 +13,8 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/results/" element={<Results />} />
+        <Route path="/freelances/" element={<Freelances />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
