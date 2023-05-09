0b3c55241f8d3c1e193e7bfcf5f098798713d979
Georges Briche <gbriche59@yahoo.fr>
2023-05-09 11:23:06 +0200

    Mise a jour code

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,9 +1,35 @@
+import Card from '../../components/Card'
+
 function Freelances() {
-    return (
+  const freelanceProfiles = [
+    {
+      name: 'Jane Doe',
+      jobTitle: 'Devops',
+    },
+    {
+      name: 'John Doe',
+      jobTitle: 'Developpeur frontend',
+    },
+    {
+      name: 'Jeanne Biche',
+      jobTitle: 'Développeuse Fullstack',
+    },
+  ]
+
+  return (
+    
       <div>
-        <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+          <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+          {freelanceProfiles.map((profile, index) => (
+              <Card
+                  key={`${profile.name}-${index}`}
+                  label={profile.jobTitle}
+                  title={profile.name}
+              />
+          ))}
       </div>
-    )
-  }
-  
-  export default Freelances
+  )
+
+}
+
+export default Freelances
