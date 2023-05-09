6bfce19d1c2822221a8bc71b0965c985b252a228
Georges Briche <gbriche59@yahoo.fr>
2023-05-09 17:19:11 +0200

    Mise a jour code

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,27 @@
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import DefaultPicture from '../../assets/defaultPicture.jpg'
+
+const CardLabel = styled.span`
+    color: #5843e4;
+    font-size: 22px;
+    font-weight: bold;
+    margin-bottom:20px;
+`
+const CardImage = styled.img`
+    height: 80px;
+    width: 80px;
+    border-radius: 50%;
+    border: 2px solid grey;
+    box-shadow: 5px 5px 10px;
+    margin-bottom: 20px;
+`
 
 function Card({ label, title, picture }) {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
-      <span>{label}</span>
-      <img src={picture} alt="freelance" height={80} width={80} />
+      <CardLabel>{label}</CardLabel>
+      <CardImage src={picture} alt="freelance" />
       <span>{title}</span>
     </div>
   )
--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,4 +1,14 @@
 import Card from '../../components/Card'
+import styled from 'styled-components'
+
+
+const CardsContainer = styled.div`
+    display: grid;
+    gap: 24px;
+    grid-template-rows: 350px 350px;
+    grid-template-columns: repeat(2, 1fr);
+`
+
 
 function Freelances() {
   const freelanceProfiles = [
