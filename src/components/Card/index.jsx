015e260008056a9bcd716fcb0cdb47316bdc11e5
Georges Briche <gbriche59@yahoo.fr>
2023-05-09 11:36:12 +0200

    Mise a jour code

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,25 +2,24 @@ import PropTypes from 'prop-types'
 import DefaultPicture from '../../assets/defaultPicture.jpg'
 
 function Card({ label, title, picture }) {
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
-            <span>{label}</span>
-            <img src={picture} alt="freelance" height={80} width={80} />
-            <span>{title}</span>
-        </div>
-    )
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
+      <span>{label}</span>
+      <img src={picture} alt="freelance" height={80} width={80} />
+      <span>{title}</span>
+    </div>
+  )
 }
 
 Card.propTypes = {
-    label: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
 }
 Card.defaultProps = {
-    title: '',
-    label: '',
-    picture: DefaultPicture
+  title: '',
+  label: '',
+  picture: DefaultPicture,
 }
- 
-export default Card
 
+export default Card
